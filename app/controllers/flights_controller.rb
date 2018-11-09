0edf1bd20af3c2680b196feffc686d4c0920d755
Paul Trin-Dinh <paultrindinh@icloud.com>
2018-11-09 20:23:18 +0100

    Added home button, changed methoed of forms to GET, and add a submit button

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,13 +1,13 @@
 class FlightsController < ApplicationController
   
+  def index
+
+  end
+
   def show
     @flights_options = Flight.all.map{|u| [u.departure_date]}.uniq
     @airports_options = Airport.all.map{|a| [a.airport_code]}
     @passengers_options = [1, 2, 3, 4]
-  end
-
-  def index
-
   end
 
 end
